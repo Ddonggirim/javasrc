f4a4d65aa0fe225932d1db59d4c4467c53075d8c
Ddonggirim <choibu02222@naver.com>
2024-02-29 17:54:37 +0900

    a

---
+++ CastingExample.java
@@ -0,0 +1,16 @@
+package Ch01;
+
+public class CastingExample {
+
+	public static void main(String[] args) {
+		int var1 = 10;
+		byte var2 = (byte)var1;
+		System.out.println(var2);
+		
+		long var3 = 300;
+		int var4 = (int)var3;
+		
+
+	}
+
+}
